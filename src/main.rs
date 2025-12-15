512f8bbdbf9824a9098e225124b7e777900d5ea0
matteo.paoli <matteo.paoli@adesso.it>
2025-12-15 18:56:42 +0100

    Add config loading with confy and example config file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(default)]
+struct Config {
+    watch_path: PathBuf,
+    root_path: PathBuf,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            watch_path: PathBuf::from("./bucket"),
+            root_path: PathBuf::from("./docs"),
+        }
+    }
+}
+
+impl Config {
+    fn load_config() -> Self {
+        let exe = std::env::current_exe().expect("current_exe() failed");
+        let exe_dir = exe.parent().unwrap_or_else(|| Path::new("."));
+        let config_path = exe_dir.join("docsort.toml");
+
+        confy::load_path(&config_path).unwrap_or_else(|err| {
+            eprintln!(
+                "Cannot read {:?} ({err}), falling back to built-in defaults",
+                config_path
+            );
+            let default = Config::default();
+            let _ = confy::store_path(&config_path, &default);
+            default
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cfg = Config::load_config();
+    let version = env!("CARGO_PKG_VERSION");
+    println!("DocDrop v{}", version);
+    println!("{cfg:?}");
 }
