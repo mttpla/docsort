09fe6db838e9557260270be577e1c778a2fea2de
mttpla <mttpla@gmail.com>
2025-12-21 00:08:41 +0100

    Refactor event filtering to use modify file filter

--- src/app/runner/processor.rs
+++ src/app/runner/processor.rs
@@ -17,7 +17,7 @@ pub fn process_messages(cfg: &Config, rx: std::sync::mpsc::Receiver<RunnerMsg>) 
             RunnerMsg::Notify(Ok(event)) => {
                 log::info!("Change: {event:?}");
                 if let Some(event) =
-                    super::filter::filter_supported_modify_metadata_extended_file_event(cfg, event)
+                    super::filter::filter_supported_modify_file_event(cfg, event)
                 {
                     log::info!("Relevant change: {event:?}");
                 }
