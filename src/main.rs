bcb4a4c2b93adf3f55bec17766c73e3f6a154dd8
mttpla <mttpla@gmail.com>
2025-12-20 18:36:33 +0100

    Move logger initialization into run block and fix autostart output functions

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,6 @@ use docsort::setup;
 use log::{error, info};
 
 fn main() {
-    let cfg = setup::config::Config::load();
-    let _logger_handle = setup::logging::init(&cfg.log_file).unwrap_or_else(|err| {
-        panic!(
-            "Unable to initialize logger at {}: {err}",
-            cfg.log_file.display()
-        )
-    });
-    let version = env!("CARGO_PKG_VERSION");
-    info!("v{version} started with config: {:?}", cfg);
 
     let cli = Cli::parse_args();
 
@@ -25,6 +16,16 @@ fn main() {
                 eprintln!("DocSort is already running!");
                 std::process::exit(1);
             });
+            let cfg = setup::config::Config::load();
+
+            let _logger_handle = setup::logging::init(&cfg.log_file).unwrap_or_else(|err| {
+                panic!(
+                    "Unable to initialize logger at {}: {err}",
+                    cfg.log_file.display()
+                )
+            });
+            let version = env!("CARGO_PKG_VERSION");
+            info!("v{version} started with config: {:?}", cfg);
             setup::folder::check_or_create_folders(&cfg).unwrap_or_else(|err| {
                 error!("Failed to create folders: {err}");
             });
@@ -35,24 +36,25 @@ fn main() {
                 error!("app terminated with error: {err:#}");
                 std::process::exit(1);
             }
+            info!("shutting down.");
         }
 
         Command::Autostart { action } => match action {
             AutostartAction::Enable => match platform::autostart::enable() {
-                Ok(()) => info!("Autostart enabled for the current user"),
+                Ok(()) => println!("Autostart enabled for the current user"),
                 Err(err) => {
-                    error!("Failed to enable autostart: {err}");
+                    eprintln!!("Failed to enable autostart: {err}");
                     std::process::exit(1);
                 }
             },
             AutostartAction::Disable => match platform::autostart::disable() {
-                Ok(()) => info!("Autostart disabled for the current user"),
+                Ok(()) => println!("Autostart disabled for the current user"),
                 Err(err) => {
-                    error!("Failed to disable autostart: {err}");
+                    eprintln!("Failed to disable autostart: {err}");
                     std::process::exit(1);
                 }
             },
         },
     }
-    info!("shutting down.");
+
 }
